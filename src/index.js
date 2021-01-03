75ae262fa70a980576c4e5e342c845d7856b658b
Jacky_Tam <jackytam97107@gmail.com>
2021-01-03 17:09:28 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
+
+
+import AdminLayout from "./layouts/Admin.jsx";
+import Login from "./pages/Login/Login";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+    <Route path="/login" component={Login} />
+      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      
+      <Redirect from="/" to="/login" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
